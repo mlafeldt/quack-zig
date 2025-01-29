7a4067b6a363b13b62ccdcf95b4e71589253718f
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2025-01-29 13:05:43 +0100

    Multi-version builds

--- build.zig
+++ build.zig
@@ -8,7 +8,11 @@ const DuckDBVersion = enum {
     @"1.1.3",
     @"1.2.0",
 
-    const default: DuckDBVersion = .@"1.1.3";
+    const all = std.enums.values(DuckDBVersion);
+
+    fn string(self: DuckDBVersion, b: *Build) []const u8 {
+        return b.fmt("v{s}", .{@tagName(self)});
+    }
 
     fn headers(self: DuckDBVersion, b: *Build) ?Build.LazyPath {
         return switch (self) {
@@ -37,7 +41,7 @@ const Platform = enum {
 
     const all = std.enums.values(Platform);
 
-    fn name(self: Platform) [:0]const u8 {
+    fn string(self: Platform) [:0]const u8 {
         return @tagName(self);
     }
 
@@ -57,9 +61,11 @@ const Platform = enum {
 
 pub fn build(b: *Build) void {
     const optimize = b.standardOptimizeOption(.{});
-    const duckdb_version = b.option(DuckDBVersion, "duckdb-version", b.fmt("DuckDB version to build for (default: {s})", .{@tagName(DuckDBVersion.default)})) orelse DuckDBVersion.default;
+    const duckdb_versions = b.option([]const DuckDBVersion, "duckdb-version", "DuckDB version(s) to build for (default: all)") orelse DuckDBVersion.all;
     const platforms = b.option([]const Platform, "platform", "DuckDB platform(s) to build for (default: all)") orelse Platform.all;
     const install_headers = b.option(bool, "install-headers", "Install DuckDB C headers") orelse false;
+
+    const test_step = b.step("test", "Run SQL logic tests");
 
     const ext_version = v: {
         var code: u8 = undefined;
@@ -76,85 +82,91 @@ pub fn build(b: *Build) void {
         break :v std.mem.trim(u8, git_describe, " \n\r");
     };
 
-    const duckdb_headers = duckdb_version.headers(b) orelse return;
     const metadata_script = b.dependency("extension_ci_tools", .{}).path("scripts/append_extension_metadata.py");
 
-    for (platforms) |platform| {
-        const target = platform.target(b);
+    for (duckdb_versions) |duckdb_version| {
+        const version_string = duckdb_version.string(b);
+        const duckdb_headers = duckdb_version.headers(b) orelse continue;
 
-        const ext = b.addSharedLibrary(.{
-            .name = "quack",
-            .target = target,
-            .optimize = optimize,
-        });
-        ext.addCSourceFiles(.{
-            .files = &.{
-                "quack_extension.c",
-            },
-            .root = b.path("src"),
-            .flags = &cflags,
-        });
-        ext.addIncludePath(duckdb_headers);
-        ext.linkLibC();
-        ext.root_module.addCMacro("DUCKDB_EXTENSION_NAME", ext.name);
-        ext.root_module.addCMacro("DUCKDB_BUILD_LOADABLE_EXTENSION", "1");
+        for (platforms) |platform| {
+            const platform_string = platform.string();
+            const target = platform.target(b);
 
-        const filename = b.fmt("{s}.duckdb_extension", .{ext.name});
-        ext.install_name = b.fmt("@rpath/{s}", .{filename}); // macOS only
+            const ext = b.addSharedLibrary(.{
+                .name = "quack",
+                .target = target,
+                .optimize = optimize,
+            });
+            ext.addCSourceFiles(.{
+                .files = &.{
+                    "quack_extension.c",
+                },
+                .root = b.path("src"),
+                .flags = &cflags,
+            });
+            ext.addIncludePath(duckdb_headers);
+            ext.linkLibC();
+            ext.root_module.addCMacro("DUCKDB_EXTENSION_NAME", ext.name);
+            ext.root_module.addCMacro("DUCKDB_BUILD_LOADABLE_EXTENSION", "1");
 
-        const ext_path = path: {
-            const cmd = b.addSystemCommand(&.{ "uv", "run", "--python=3.12" });
-            cmd.addFileArg(metadata_script);
-            cmd.addArgs(&.{ "--extension-name", ext.name });
-            cmd.addArgs(&.{ "--extension-version", ext_version });
-            cmd.addArgs(&.{ "--duckdb-platform", platform.name() });
-            cmd.addArgs(&.{ "--duckdb-version", duckdb_version.extensionAPIVersion() });
-            cmd.addArg("--library-file");
-            cmd.addArtifactArg(ext);
-            cmd.addArg("--out-file");
-            const path = cmd.addOutputFileArg(filename);
+            const filename = b.fmt("{s}.duckdb_extension", .{ext.name});
+            ext.install_name = b.fmt("@rpath/{s}", .{filename}); // macOS only
 
-            cmd.step.name = b.fmt("add metadata {s}", .{platform.name()});
-            break :path path;
-        };
+            const ext_path = path: {
+                const cmd = b.addSystemCommand(&.{ "uv", "run", "--python=3.12" });
+                cmd.addFileArg(metadata_script);
+                cmd.addArgs(&.{ "--extension-name", ext.name });
+                cmd.addArgs(&.{ "--extension-version", ext_version });
+                cmd.addArgs(&.{ "--duckdb-platform", platform_string });
+                cmd.addArgs(&.{ "--duckdb-version", duckdb_version.extensionAPIVersion() });
+                cmd.addArg("--library-file");
+                cmd.addArtifactArg(ext);
+                cmd.addArg("--out-file");
+                const path = cmd.addOutputFileArg(filename);
 
-        const install_file = b.addInstallFileWithDir(ext_path, .{ .custom = platform.name() }, filename);
-        install_file.step.name = b.fmt("install {s}/{s}", .{ platform.name(), filename });
-        b.getInstallStep().dependOn(&install_file.step);
-
-        if (install_headers) {
-            const header_dirs = [_]Build.LazyPath{
-                duckdb_headers,
-                // Add more header directories here
+                cmd.step.name = b.fmt("metadata {s} {s}", .{ version_string, platform_string });
+                break :path path;
             };
-            for (header_dirs) |dir| {
-                b.getInstallStep().dependOn(&b.addInstallDirectory(.{
-                    .source_dir = dir,
-                    .include_extensions = &.{"h"},
-                    .install_dir = .header,
-                    .install_subdir = "",
-                }).step);
+
+            const install_file = b.addInstallFileWithDir(ext_path, .{
+                .custom = b.fmt("{s}/{s}", .{ version_string, platform_string }),
+            }, filename);
+            install_file.step.name = b.fmt("install {s} {s}", .{ version_string, platform_string });
+            b.getInstallStep().dependOn(&install_file.step);
+
+            if (install_headers) {
+                const header_dirs = [_]Build.LazyPath{
+                    duckdb_headers,
+                    // Add more header directories here
+                };
+                for (header_dirs) |dir| {
+                    b.getInstallStep().dependOn(&b.addInstallDirectory(.{
+                        .source_dir = dir,
+                        .include_extensions = &.{"h"},
+                        .install_dir = .{ .custom = version_string },
+                        .install_subdir = "include",
+                    }).step);
+                }
             }
-        }
 
-        // Run tests on native platform
-        if (b.host.result.os.tag == target.result.os.tag and
-            b.host.result.cpu.arch == target.result.cpu.arch and
-            !b.top_level_steps.contains("test")) // HACK: Avoid adding step twice, e.g. for linux_amd64 and linux_amd64_gcc4
-        {
-            const sqllogictest = b.lazyDependency("sqllogictest", .{}) orelse continue;
+            // Run tests on native platform
+            if (b.host.result.os.tag == target.result.os.tag and
+                b.host.result.cpu.arch == target.result.cpu.arch and
+                duckdb_version != .@"1.2.0") // TODO: Remove once Python package is available
+            {
+                const sqllogictest = b.lazyDependency("sqllogictest", .{}) orelse continue;
 
-            const cmd = b.addSystemCommand(&.{ "uv", "run", "--python=3.12", "--with" });
-            cmd.addFileArg(sqllogictest.path(""));
-            cmd.addArgs(&.{ "--with", b.fmt("duckdb=={s}", .{@tagName(duckdb_version)}) });
-            cmd.addArgs(&.{ "python3", "-m", "duckdb_sqllogictest" });
-            cmd.addArgs(&.{ "--test-dir", "test" });
-            cmd.addArg("--external-extension");
-            cmd.addFileArg(ext_path);
-            cmd.step.name = "sqllogictest";
+                const cmd = b.addSystemCommand(&.{ "uv", "run", "--python=3.12", "--with" });
+                cmd.addFileArg(sqllogictest.path(""));
+                cmd.addArgs(&.{ "--with", b.fmt("duckdb=={s}", .{@tagName(duckdb_version)}) });
+                cmd.addArgs(&.{ "python3", "-m", "duckdb_sqllogictest" });
+                cmd.addArgs(&.{ "--test-dir", "test" });
+                cmd.addArg("--external-extension");
+                cmd.addFileArg(ext_path);
+                cmd.step.name = b.fmt("sqllogictest {s} {s}", .{ version_string, platform_string });
 
-            const test_step = b.step("test", "Run SQL logic tests");
-            test_step.dependOn(&cmd.step);
+                test_step.dependOn(&cmd.step);
+            }
         }
     }
 }
