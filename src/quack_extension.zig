010bd37c38c0dae5b45696f66012f979bf22b0a6
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2025-02-14 13:04:18 +0100

    WIP

--- src/quack_extension.zig
+++ src/quack_extension.zig
@@ -3,7 +3,7 @@ const allocator = std.heap.raw_c_allocator;
 
 const Extension = @import("extension.zig");
 const ScalarFunction = Extension.ScalarFunction;
-const api = &Extension.api;
+const D = &Extension.api;
 const c = Extension.c;
 
 export fn quack_init_c_api(info: c.duckdb_extension_info, access: *c.duckdb_extension_access) bool {
@@ -27,24 +27,24 @@ fn quackFunction(_: c.duckdb_function_info, input: c.duckdb_data_chunk, output: 
     const quack_prefix = "Quack ";
     const quack_suffix = " 🐥";
 
-    const input_vector = api.duckdb_data_chunk_get_vector.?(input, 0);
-    const input_data: [*]c.duckdb_string_t = @ptrCast(@alignCast(api.duckdb_vector_get_data.?(input_vector)));
-    const input_mask = api.duckdb_vector_get_validity.?(input_vector);
+    const input_vector = D.duckdb_data_chunk_get_vector.?(input, 0);
+    const input_data: [*]c.duckdb_string_t = @ptrCast(@alignCast(D.duckdb_vector_get_data.?(input_vector)));
+    const input_mask = D.duckdb_vector_get_validity.?(input_vector);
 
-    api.duckdb_vector_ensure_validity_writable.?(output);
-    const result_mask = api.duckdb_vector_get_validity.?(output);
+    D.duckdb_vector_ensure_validity_writable.?(output);
+    const result_mask = D.duckdb_vector_get_validity.?(output);
 
-    const num_rows = api.duckdb_data_chunk_get_size.?(input);
+    const num_rows = D.duckdb_data_chunk_get_size.?(input);
     for (0..num_rows) |row| {
-        if (!api.duckdb_validity_row_is_valid.?(input_mask, row)) {
+        if (!D.duckdb_validity_row_is_valid.?(input_mask, row)) {
             // name is NULL -> set result to NULL
-            api.duckdb_validity_set_row_invalid.?(result_mask, row);
+            D.duckdb_validity_set_row_invalid.?(result_mask, row);
             continue;
         }
 
         var name = input_data[row];
-        const name_str = api.duckdb_string_t_data.?(&name);
-        const name_len = api.duckdb_string_t_length.?(name);
+        const name_str = D.duckdb_string_t_data.?(&name);
+        const name_len = D.duckdb_string_t_length.?(name);
 
         const result_str = std.mem.concat(allocator, u8, &[_][]const u8{
             quack_prefix,
@@ -52,7 +52,7 @@ fn quackFunction(_: c.duckdb_function_info, input: c.duckdb_data_chunk, output: 
             quack_suffix,
         }) catch @panic("OOM");
 
-        api.duckdb_vector_assign_string_element_len.?(output, row, @ptrCast(result_str), result_str.len);
+        D.duckdb_vector_assign_string_element_len.?(output, row, @ptrCast(result_str), result_str.len);
         allocator.free(result_str);
     }
 }
