1a273705d5fe0d801826d8662285d737d0a70d2a
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2025-02-20 15:20:49 +0100

    Match GCC4 ABI by targeting manylinux2014's glibc version

--- build.zig
+++ build.zig
@@ -1,64 +1,6 @@
 const std = @import("std");
 const Build = std.Build;
 
-const DuckDBVersion = enum {
-    @"1.1.0", // First version with C API support
-    @"1.1.1",
-    @"1.1.2",
-    @"1.1.3",
-    @"1.2.0",
-
-    const all = std.enums.values(@This());
-
-    fn toString(self: @This(), b: *Build) []const u8 {
-        return b.fmt("v{s}", .{@tagName(self)});
-    }
-
-    fn headers(self: @This(), b: *Build) Build.LazyPath {
-        return switch (self) {
-            .@"1.1.0", .@"1.1.1", .@"1.1.2", .@"1.1.3" => b.dependency("libduckdb_1_1_3", .{}).path(""),
-            .@"1.2.0" => b.dependency("libduckdb_1_2_0", .{}).path(""),
-        };
-    }
-
-    fn extensionAPIVersion(self: @This()) [:0]const u8 {
-        return switch (self) {
-            .@"1.1.0", .@"1.1.1", .@"1.1.2", .@"1.1.3" => "v0.0.1",
-            .@"1.2.0" => "v1.2.0",
-        };
-    }
-};
-
-const Platform = enum {
-    linux_amd64, // Node.js packages, etc.
-    linux_amd64_gcc4, // Python packages, CLI, etc.
-    linux_arm64,
-    linux_arm64_gcc4,
-    osx_amd64,
-    osx_arm64,
-    windows_amd64,
-    windows_arm64,
-
-    const all = std.enums.values(@This());
-
-    fn toString(self: @This()) [:0]const u8 {
-        return @tagName(self);
-    }
-
-    fn target(self: @This(), b: *Build) Build.ResolvedTarget {
-        return b.resolveTargetQuery(switch (self) {
-            .linux_amd64 => .{ .os_tag = .linux, .cpu_arch = .x86_64, .abi = .gnu },
-            .linux_amd64_gcc4 => .{ .os_tag = .linux, .cpu_arch = .x86_64, .abi = .gnu }, // TODO: Set glibc_version?
-            .linux_arm64 => .{ .os_tag = .linux, .cpu_arch = .aarch64, .abi = .gnu },
-            .linux_arm64_gcc4 => .{ .os_tag = .linux, .cpu_arch = .aarch64, .abi = .gnu }, // TODO: Set glibc_version?
-            .osx_amd64 => .{ .os_tag = .macos, .cpu_arch = .x86_64, .abi = .none },
-            .osx_arm64 => .{ .os_tag = .macos, .cpu_arch = .aarch64, .abi = .none },
-            .windows_amd64 => .{ .os_tag = .windows, .cpu_arch = .x86_64, .abi = .gnu },
-            .windows_arm64 => .{ .os_tag = .windows, .cpu_arch = .aarch64, .abi = .gnu },
-        });
-    }
-};
-
 pub fn build(b: *Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const duckdb_versions = b.option([]const DuckDBVersion, "duckdb-version", "DuckDB version(s) to build for (default: all)") orelse DuckDBVersion.all;
@@ -163,6 +105,100 @@ pub fn build(b: *Build) void {
         }
     }
 }
+
+const DuckDBVersion = enum {
+    @"1.1.0", // First version with C API support
+    @"1.1.1",
+    @"1.1.2",
+    @"1.1.3",
+    @"1.2.0",
+
+    const all = std.enums.values(@This());
+
+    fn toString(self: @This(), b: *Build) []const u8 {
+        return b.fmt("v{s}", .{@tagName(self)});
+    }
+
+    fn headers(self: @This(), b: *Build) Build.LazyPath {
+        return switch (self) {
+            .@"1.1.0", .@"1.1.1", .@"1.1.2", .@"1.1.3" => b.dependency("libduckdb_1_1_3", .{}).path(""),
+            .@"1.2.0" => b.dependency("libduckdb_1_2_0", .{}).path(""),
+        };
+    }
+
+    fn extensionAPIVersion(self: @This()) [:0]const u8 {
+        return switch (self) {
+            .@"1.1.0", .@"1.1.1", .@"1.1.2", .@"1.1.3" => "v0.0.1",
+            .@"1.2.0" => "v1.2.0",
+        };
+    }
+};
+
+const Platform = enum {
+    linux_amd64, // Node.js packages, etc.
+    linux_amd64_gcc4, // Python packages, CLI, etc.
+    linux_arm64,
+    linux_arm64_gcc4,
+    osx_amd64,
+    osx_arm64,
+    windows_amd64,
+    windows_arm64,
+
+    const all = std.enums.values(@This());
+
+    fn toString(self: @This()) [:0]const u8 {
+        return @tagName(self);
+    }
+
+    fn target(self: @This(), b: *Build) Build.ResolvedTarget {
+        const manylinux2014_glibc_version = std.SemanticVersion{ .major = 2, .minor = 17, .patch = 0 };
+
+        return b.resolveTargetQuery(switch (self) {
+            .linux_amd64 => .{
+                .os_tag = .linux,
+                .cpu_arch = .x86_64,
+                .abi = .gnu,
+            },
+            .linux_amd64_gcc4 => .{
+                .os_tag = .linux,
+                .cpu_arch = .x86_64,
+                .abi = .gnu,
+                .glibc_version = manylinux2014_glibc_version,
+            },
+            .linux_arm64 => .{
+                .os_tag = .linux,
+                .cpu_arch = .aarch64,
+                .abi = .gnu,
+            },
+            .linux_arm64_gcc4 => .{
+                .os_tag = .linux,
+                .cpu_arch = .aarch64,
+                .abi = .gnu,
+                .glibc_version = manylinux2014_glibc_version,
+            },
+            .osx_amd64 => .{
+                .os_tag = .macos,
+                .cpu_arch = .x86_64,
+                .abi = .none,
+            },
+            .osx_arm64 => .{
+                .os_tag = .macos,
+                .cpu_arch = .aarch64,
+                .abi = .none,
+            },
+            .windows_amd64 => .{
+                .os_tag = .windows,
+                .cpu_arch = .x86_64,
+                .abi = .gnu, // TODO: Switch to msvc?
+            },
+            .windows_arm64 => .{
+                .os_tag = .windows,
+                .cpu_arch = .aarch64,
+                .abi = .gnu, // TODO: Switch to msvc?
+            },
+        });
+    }
+};
 
 fn detectGitVersion(b: *std.Build) ![]const u8 {
     var code: u8 = 0;
